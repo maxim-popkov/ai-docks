3152238af449145653d81a92407d944b7cd1ff7a
Maxim <maxim.popkov.91@gmail.com>
2023-08-24 23:04:41 +0300

    FIX install llms

--- toDocker/install/install-llms.sh
+++ toDocker/install/install-llms.sh
@@ -3,4 +3,4 @@ pip install auto-gptq
 pip install -q -U trl transformers accelerate git+https://github.com/huggingface/peft.git
 pip install -q datasets einops
 pip uninstall bitsandbytes
-pip install bitsandbytes
+pip install -q bitsandbytes
