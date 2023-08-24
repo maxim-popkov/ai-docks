499599c39b89ec2b4cf5f17e0f7b5aa9bb0e838f
Maxim <maxim.popkov.91@gmail.com>
2023-08-24 22:07:51 +0300

    ADD ld path for curl

--- toDocker/install/pre-install.sh
+++ toDocker/install/pre-install.sh
@@ -1,3 +1,3 @@
 # export LD_LIBRARY_PATH=/home/popkov-mi/miniconda3/envs/art-env/lib:$LD_LIBRARY_PATH
-echo 'export LD_LIBRARY_PATH=/home/popkov-mi/miniconda3/envs/art-env/lib:$LD_LIBRARY_PATH' >> /home/popkov-mi/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/home/popkov-mi/miniconda3/envs/art-env/lib:$LD_LIBRARY_PATH' >> /home/popkov-mi/.bashrc
 source ~/.bashrc
