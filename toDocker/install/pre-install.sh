40bcab3ab3e53270db7f9df95482f48ac21b8b7f
Maxim <maxim.popkov.91@gmail.com>
2023-08-24 23:08:14 +0300

    FIX per install hs

--- toDocker/install/pre-install.sh
+++ toDocker/install/pre-install.sh
@@ -1,3 +1,3 @@
 # export LD_LIBRARY_PATH=/home/popkov-mi/miniconda3/envs/art-env/lib:$LD_LIBRARY_PATH
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.4/targets/x86_64-linux/lib:/home/popkov-mi/miniconda3/envs/art-env/lib:$LD_LIBRARY_PATH' >> /home/popkov-mi/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.4/targets/x86_64-linux/lib:/usr/lib/x86_64-linux-gnu:/home/popkov-mi/miniconda3/envs/art-env/lib:$LD_LIBRARY_PATH' >> /home/popkov-mi/.bashrc
 source ~/.bashrc
