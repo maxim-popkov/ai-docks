ddd44f16912ee6eb9985ffa64ef882ea0caa3dea
Maxim <maxim.popkov.91@gmail.com>
2023-08-24 23:02:01 +0300

    ADD install llms

---
+++ toDocker/install/install-llms.sh
@@ -0,0 +1,6 @@
+conda install cudatoolkit
+pip install auto-gptq
+pip install -q -U trl transformers accelerate git+https://github.com/huggingface/peft.git
+pip install -q datasets einops
+pip uninstall bitsandbytes
+pip install bitsandbytes
