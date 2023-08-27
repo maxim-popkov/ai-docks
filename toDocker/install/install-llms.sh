2d6400531493b5ab49eca047848ee25eee583abf
Maxim <maxim.popkov.91@gmail.com>
2023-08-27 19:29:46 +0300

    FIX install lmms

--- toDocker/install/install-llms.sh
+++ toDocker/install/install-llms.sh
@@ -1,6 +1,6 @@
-conda install cudatoolkit
+conda install -y cudatoolkit
+pip uninstall bitsandbytes
 pip install auto-gptq
 pip install -q -U trl transformers accelerate git+https://github.com/huggingface/peft.git
 pip install -q datasets einops
-pip uninstall bitsandbytes
 pip install -q bitsandbytes
