9fe7cee6ab8569242ba991dcda3df6efde23621b
Maxim <maxim.popkov.91@gmail.com>
2023-10-26 18:07:31 +0300

    ADD comment for unpack

--- toDocker/utils/zip-comfy.sh
+++ toDocker/utils/zip-comfy.sh
@@ -10,3 +10,5 @@ mkdir -p ${DESTINATION_DIR}
 
 # Create tar archives and split them if size is more than 2GB
 cd ${PARENT_DIR} && tar -cvpf - ${DIR_NAME} | gzip | split -b 2G - ${DESTINATION_DIR}comfy_app_dir.tar.gz.
+
+#cat /home/popkov-mi/archives/comfy_app_dir.tar.gz.* | gzip -d | tar -xvf - -C /home/popkov-mi/archives/comfy_app
