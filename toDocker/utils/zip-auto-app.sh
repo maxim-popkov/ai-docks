c9e9085ca188aee4cbffd2a895611eb6411b11ac
Maxim <maxim.popkov.91@gmail.com>
2023-10-26 17:57:51 +0300

    FIX zip routins

--- toDocker/utils/zip-auto-app.sh
+++ toDocker/utils/zip-auto-app.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Paths
-PARENT_DIR="/home/popkov-mi/miniconda3/envs"
-DIR_NAME="art-env"
+PARENT_DIR="/home/popkov-mi/sd-app/v1"
+DIR_NAME="auto"
 
 # Destination directory
 DESTINATION_DIR="/home/popkov-mi/archives/"
 mkdir -p ${DESTINATION_DIR}
 
 # Create tar archives and split them if size is more than 2GB
-cd ${PARENT_DIR} && tar -cvpf - ${DIR_NAME} | gzip | split -b 2G - ${DESTINATION_DIR}miniconda_dir.tar.gz.
+cd ${PARENT_DIR} && tar -cvpf - ${DIR_NAME} | gzip | split -b 2G - ${DESTINATION_DIR}sd_app_dir.tar.gz.
+
--- toDocker/utils/zip-comfy.sh
+++ toDocker/utils/zip-comfy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Paths
-PARENT_DIR="/home/popkov-mi/miniconda3/envs"
-DIR_NAME="art-env"
+PARENT_DIR="/home/popkov-mi"
+DIR_NAME="comfy-app"
 
 # Destination directory
 DESTINATION_DIR="/home/popkov-mi/archives/"
 mkdir -p ${DESTINATION_DIR}
 
 # Create tar archives and split them if size is more than 2GB
-cd ${PARENT_DIR} && tar -cvpf - ${DIR_NAME} | gzip | split -b 2G - ${DESTINATION_DIR}miniconda_dir.tar.gz.
+cd ${PARENT_DIR} && tar -cvpf - ${DIR_NAME} | gzip | split -b 2G - ${DESTINATION_DIR}comfy_app_dir.tar.gz.
