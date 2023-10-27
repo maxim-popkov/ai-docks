21eb48a62479e421e4b59571684f576f8f3ab690
Maxim <maxim.popkov.91@gmail.com>
2023-10-27 16:56:08 +0300

    ADD LoRA for example

--- toDocker/install/install-comfyui.sh
+++ toDocker/install/install-comfyui.sh
@@ -6,6 +6,8 @@ git clone https://github.com/comfyanonymous/ComfyUI.git ${COMFY_HOME}
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git ${COMFY_HOME}/custom_nodes/ComfyUI-Manager
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux/ ${COMFY_HOME}/custom_nodes/comfyui_controlnet_aux
 git clone https://github.com/RockOfFire/ComfyUI_Comfyroll_CustomNodes.git ${COMFY_HOME}/custom_nodes/ComfyUI_Comfyroll_CustomNodes
+git clone https://github.com/WASasquatch/was-node-suite-comfyui/ ${COMFY_HOME}/custom_nodes/was-node-suite-comfyui
+
 
 # SeargeSDXL WorkFlow
 git clone https://github.com/SeargeDP/SeargeSDXL.git ${COMFY_HOME}/custom_nodes/SeargeSDXL
@@ -14,9 +16,13 @@ git clone https://github.com/SeargeDP/SeargeSDXL.git ${COMFY_HOME}/custom_nodes/
 pip install -q -r ${COMFY_HOME}/requirements.txt
 pip install -q timm==0.6.7
 pip install -r ${COMFY_HOME}/custom_nodes/comfyui_controlnet_aux/requirements.txt
+pip install -r ${COMFY_HOME}/custom_nodes/was-node-suite-comfyui/requirements.txt
 
 # deliberate
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors -d ${COMFY_HOME}/models/checkpoints -o Deliberate_v2.safetensors
+aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors -d ${COMFY_HOME}/models/loras -o sd_xl_offset_example-lora_1.0.safetensors
+
+
 
 # sdxl
 # aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors -d ${COMFY_HOME}/models/checkpoints -o sd_xl_base_1.0.safetensors
