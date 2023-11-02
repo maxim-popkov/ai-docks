cffce6df89ed464ef0cdb8026a93c2d5dcc6f0d8
Maxim <maxim.popkov.91@gmail.com>
2023-11-02 16:57:40 +0300

    ADD repo LoRA fix in install

--- toDocker/install/install-lora.sh
+++ toDocker/install/install-lora.sh
@@ -13,3 +13,11 @@ wget https://raw.githubusercontent.com/hollowstrawberry/kohya-colab/xformers-fix
 
 pip install --upgrade -r requirements.txt
 pip install xformers==0.0.22.post4 --index-url https://download.pytorch.org/whl/cu118
+
+
+sed -i "s@cpu@cuda@" library/model_util.py
+# позволяет загружать обрезанные изображения без вызова ошибки
+sed -i 's/from PIL import Image/from PIL import Image, ImageFile\nImageFile.LOAD_TRUNCATED_IMAGES=True/g' library/train_util.py # fix truncated jpegs error
+# сделать симпотичнее именование эпох
+sed -i 's/{:06d}/{:02d}/g' library/train_util.py # make epoch names shorter
+sed -i 's/"." + args.save_model_as)/"-{:02d}.".format(num_train_epochs) + args.save_model_as)/g' train_network.py # name of the last epoch will match the rest
