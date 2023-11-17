c40deb4fbd5fc0894bb3584fbd1882f57da2ee29
Maxim <maxim.popkov.91@gmail.com>
2023-11-17 22:45:15 +0300

    ADD comment for symlink

--- toDocker/setup/install_requirements.sh
+++ toDocker/setup/install_requirements.sh
@@ -6,4 +6,5 @@ mamba install -y cudatoolkit
 
 # Создаем символическую ссылку для libstdc++.so.6
 ln -sf /home/popkov-mi/miniconda3/envs/art-env/lib/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+# Сделать ссылку неизменямой (нужно для vast.ai, vast.ai при старте докера портит эту зависимость)
 chattr +i /usr/lib/x86_64-linux-gnu/libstdc++.so.6
