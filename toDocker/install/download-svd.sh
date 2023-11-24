c01868df94e1a4ad8270b657e7dc714cd91cd60e
maxim-popkov <maxim.popkov.91@gmail.com>
2023-11-24 23:20:18 +0300

    FIX comfyui svd path

--- toDocker/install/download-svd.sh
+++ toDocker/install/download-svd.sh
@@ -4,6 +4,7 @@ source .env
 SVD_MODELS="/home/popkov-mi/sd-app/svd-models"
 
 mkdir -p ${SVD_MODELS}
+mkdir -p ${COMFY_HOME}/models/svd/
 
 # svd models
 aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd.safetensors -d ${SVD_MODELS} -o svd.safetensors
